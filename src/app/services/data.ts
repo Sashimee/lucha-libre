74aeceb31011be9b92b65f05f681ccf61929054d
GOStephano <60096607+GOStephano@users.noreply.github.com>
2020-03-19 12:58:02 +0100

    Update data.ts

---
+++ src/app/services/data.ts
@@ -0,0 +1,12 @@
+export const wreslerArray = [
+  {
+    name: "Abismo Negro",
+    dateOfBirth: "01/06/1971",
+    height: 1.8,
+    weight: 96,
+    debut: 1987,
+    rentPrice: 0,
+    specialMove: "Flamme Thrower",
+    wreslerId: "1"
+  }
+];
