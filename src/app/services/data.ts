e3f9f37ee02ad86250ccb47fc9ea624672d287f3
Alex <60114079+Sashimee@users.noreply.github.com>
2020-03-19 14:28:07 +0100

    commit

--- src/app/services/data.ts
+++ src/app/services/data.ts
@@ -1,136 +1,136 @@
 export const wreslerArray = [
-  {
-    name: "Abismo Negro",
-    dateOfBirth: "01/06/1971",
-    height: 1.8,
-    weight: 96,
-    debut: 1987,
-    rentPrice: 0,
-    specialMove: "Flamme Thrower",
-    image: "../assets/wreslers/Abismo_Negro_En_Mexicali.JPG",
-    wreslerId: "1"
-  },
-  {
-    name: "Atlantis ",
-    dateOfBirth: "28/09/1962",
-    height: 1.73,
-    weight: 82,
-    debut: 1983,
-    rentPrice: 100,
-    specialMove: "Double Back flip head kick",
-    image:
-      "../assets/wreslers/330px-MX_TV_PLACA_PATRIMONIO_LUCHA_LIBRE_(45220918455).jpg",
-    wreslerId: "2"
-  },
-  {
-    name: "Blue Demon",
-    dateOfBirth: "24/04/1922",
-    height: 1.69,
-    weight: 77,
-    debut: 1948,
-    rentPrice: 330,
-    specialMove: "Ice storm",
-    image: "../assets/wreslers/BlueDemonPose.JPG",
-    wreslerId: "3"
-  },
-  {
-    name: "Dr. Wagner",
-    dateOfBirth: "13/04/1936",
-    height: 1.74,
-    weight: 94,
-    debut: 1287,
-    rentPrice: 22000,
-    specialMove: "La Ola Blanca",
-    image: "../assets/wreslers/Dr._Wagner_1973.JPG",
-    wreslerId: "4"
-  },
-  {
-    name: "Dr. Wagner Jr.",
-    dateOfBirth: "01/06/1965",
-    height: 1.77,
-    weight: 98,
-    debut: 1985,
-    rentPrice: 1000,
-    specialMove: "Seven deadly punch",
-    image: "../assets/wreslers/Dr._Wagner,_Jr._Latin_American_Champion.JPG",
-    wreslerId: "5"
-  },
-  {
-    name: "Doctor X",
-    dateOfBirth: "17/06/1968",
-    height: 1.73,
-    weight: 80,
-    debut: 1995,
-    rentPrice: 290,
-    specialMove: "Fairy Jump",
-    image: "../assets/wreslers/Dr_X_luchador.JPG",
-    wreslerId: "6"
-  },
-  {
-    name: "Dos Caras",
-    dateOfBirth: "21/02/1951",
-    height: 1.78,
-    weight: 96,
-    debut: 1970,
-    rentPrice: 32320,
-    specialMove: "Two Face blow",
-    image: "../assets/wreslers/330px-Dos_Caras.JPG",
-    wreslerId: "7"
-  },
-  {
-    name: "Canek",
-    dateOfBirth: "19/06/1952",
-    height: 1.83,
-    weight: 110,
-    debut: 1972,
-    rentPrice: 0.0,
-    specialMove: "The power of words",
-    image: "../assets/wreslers/330px-El_Canek_2018.JPG",
-    wreslerId: "8"
-  },
-  {
-    name: "El Santo",
-    dateOfBirth: "23/09/1917",
-    height: 1.78,
-    weight: 95,
-    debut: 1934,
-    rentPrice: 12010,
-    specialMove: "Triple Back front double flip kick jump",
-    image: "../assets/wreslers/330px-El_Santo_statue.JPG",
-    wreslerId: "9"
-  },
-  {
-    name: "El Hijo del Santo",
-    dateOfBirth: "17/02/1963",
-    height: 1.63,
-    weight: 82,
-    debut: 1982,
-    rentPrice: 320.0,
-    specialMove: "Cant be bothered anymore",
-    image: "../assets/wreslers/Hijo_del_Santo.JPG",
-    wreslerId: "10"
-  },
-  {
-    name: "El Solitario",
-    dateOfBirth: "06/06/1986",
-    height: 2.3,
-    weight: 170,
-    debut: 1960,
-    rentPrice: 123450.0,
-    specialMove: "Fat kid sitting on your face",
-    image:
-      "../assets/wreslers/El_Solitario_with_the_UWA_World_Cruiserweight_Champion.JPG",
-    wreslerId: "11"
-  },
-  {
-    name: "Fishman ",
-    dateOfBirth: "02/11/1926",
-    height: 1.1,
-    weight: 35,
-    debut: 1890,
-    rentPrice: 123450.0,
-    specialMove: "Dead man blow",
-    image: "../assets/wreslers/330px-Fishman_Mask.JPG",
-    wreslerId: "12"
-  }
+    {
+        name: "Abismo Negro",
+        dateOfBirth: "01/06/1971",
+        height: 1.8,
+        weight: 96,
+        debut: 1987,
+        rentPrice: 0,
+        specialMove: "Flamme Thrower",
+        image: "../../assets/wreslers/Abismo_Negro_En_Mexicali.JPG",
+        wreslerId: "1"
+    },
+    {
+        name: "Atlantis ",
+        dateOfBirth: "28/09/1962",
+        height: 1.73,
+        weight: 82,
+        debut: 1983,
+        rentPrice: 100,
+        specialMove: "Double Back flip head kick",
+        image:
+            "../../assets/wreslers/330px-MX_TV_PLACA_PATRIMONIO_LUCHA_LIBRE_(45220918455).jpg",
+        wreslerId: "2"
+    },
+    {
+        name: "Blue Demon",
+        dateOfBirth: "24/04/1922",
+        height: 1.69,
+        weight: 77,
+        debut: 1948,
+        rentPrice: 330,
+        specialMove: "Ice storm",
+        image: "../../assets/wreslers/BlueDemonPose.JPG",
+        wreslerId: "3"
+    },
+    {
+        name: "Dr. Wagner",
+        dateOfBirth: "13/04/1936",
+        height: 1.74,
+        weight: 94,
+        debut: 1287,
+        rentPrice: 22000,
+        specialMove: "La Ola Blanca",
+        image: "../../assets/wreslers/Dr._Wagner_1973.JPG",
+        wreslerId: "4"
+    },
+    {
+        name: "Dr. Wagner Jr.",
+        dateOfBirth: "01/06/1965",
+        height: 1.77,
+        weight: 98,
+        debut: 1985,
+        rentPrice: 1000,
+        specialMove: "Seven deadly punch",
+        image: "../../assets/wreslers/Dr._Wagner,_Jr._Latin_American_Champion.JPG",
+        wreslerId: "5"
+    },
+    {
+        name: "Doctor X",
+        dateOfBirth: "17/06/1968",
+        height: 1.73,
+        weight: 80,
+        debut: 1995,
+        rentPrice: 290,
+        specialMove: "Fairy Jump",
+        image: "../../assets/wreslers/Dr_X_luchador.JPG",
+        wreslerId: "6"
+    },
+    {
+        name: "Dos Caras",
+        dateOfBirth: "21/02/1951",
+        height: 1.78,
+        weight: 96,
+        debut: 1970,
+        rentPrice: 32320,
+        specialMove: "Two Face blow",
+        image: "../../assets/wreslers/330px-Dos_Caras.JPG",
+        wreslerId: "7"
+    },
+    {
+        name: "Canek",
+        dateOfBirth: "19/06/1952",
+        height: 1.83,
+        weight: 110,
+        debut: 1972,
+        rentPrice: 0.0,
+        specialMove: "The power of words",
+        image: "../../assets/wreslers/330px-El_Canek_2018.JPG",
+        wreslerId: "8"
+    },
+    {
+        name: "El Santo",
+        dateOfBirth: "23/09/1917",
+        height: 1.78,
+        weight: 95,
+        debut: 1934,
+        rentPrice: 12010,
+        specialMove: "Triple Back front double flip kick jump",
+        image: "../../assets/wreslers/330px-El_Santo_statue.JPG",
+        wreslerId: "9"
+    },
+    {
+        name: "El Hijo del Santo",
+        dateOfBirth: "17/02/1963",
+        height: 1.63,
+        weight: 82,
+        debut: 1982,
+        rentPrice: 320.0,
+        specialMove: "Cant be bothered anymore",
+        image: "../../assets/wreslers/Hijo_del_Santo.JPG",
+        wreslerId: "10"
+    },
+    {
+        name: "El Solitario",
+        dateOfBirth: "06/06/1986",
+        height: 2.3,
+        weight: 170,
+        debut: 1960,
+        rentPrice: 123450.0,
+        specialMove: "Fat kid sitting on your face",
+        image:
+            "../../assets/wreslers/El_Solitario_with_the_UWA_World_Cruiserweight_Champion.JPG",
+        wreslerId: "11"
+    },
+    {
+        name: "Fishman ",
+        dateOfBirth: "02/11/1926",
+        height: 1.1,
+        weight: 35,
+        debut: 1890,
+        rentPrice: 123450.0,
+        specialMove: "Dead man blow",
+        image: "../../assets/wreslers/330px-Fishman_Mask.JPG",
+        wreslerId: "12"
+    }
 ];
