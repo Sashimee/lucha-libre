4377e6eb40203f826e3047416d5e77d8294e888e
GOStephano <60096607+GOStephano@users.noreply.github.com>
2020-03-19 20:10:51 +0100

    Calendartest

--- src/app/components/page-match-calendar/page-match-calendar.component.ts
+++ src/app/components/page-match-calendar/page-match-calendar.component.ts
@@ -1,15 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-page-match-calendar',
-  templateUrl: './page-match-calendar.component.html',
-  styleUrls: ['./page-match-calendar.component.css']
+  selector: "app-page-match-calendar",
+  templateUrl: "./page-match-calendar.component.html",
+  styleUrls: ["./page-match-calendar.component.scss"]
 })
 export class PageMatchCalendarComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
