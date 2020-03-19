ebc9888e05d4d8e961da4672d1eb7a1f42aaf169
GOStephano <60096607+GOStephano@users.noreply.github.com>
2020-03-19 14:11:29 +0100

    wreslers list

--- src/app/components/page-wresler-details/page-wresler-details.component.ts
+++ src/app/components/page-wresler-details/page-wresler-details.component.ts
@@ -1,15 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { wreslerArray } from "../../services/data";
 
 @Component({
-  selector: 'app-page-wresler-details',
-  templateUrl: './page-wresler-details.component.html',
-  styleUrls: ['./page-wresler-details.component.css']
+  selector: "app-page-wresler-details",
+  templateUrl: "./page-wresler-details.component.html",
+  styleUrls: ["./page-wresler-details.component.css"]
 })
 export class PageWreslerDetailsComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
--- src/app/components/page-wreslers-list/page-wreslers-list.component.ts
+++ src/app/components/page-wreslers-list/page-wreslers-list.component.ts
@@ -1,15 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { wreslerArray } from "../../services/data";
 
 @Component({
-  selector: 'app-page-wreslers-list',
-  templateUrl: './page-wreslers-list.component.html',
-  styleUrls: ['./page-wreslers-list.component.css']
+  selector: "app-page-wreslers-list",
+  templateUrl: "./page-wreslers-list.component.html",
+  styleUrls: ["./page-wreslers-list.component.css"]
 })
 export class PageWreslersListComponent implements OnInit {
+  wreslerList = wreslerArray;
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
