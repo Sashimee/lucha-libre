0e2ce6a083c4e32ed74c2adae4d504b12b167679
Alex <60114079+Sashimee@users.noreply.github.com>
2020-03-19 13:54:59 +0100

    Update app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { PageDashboardComponent } from './components/page-dashboard/page-dashboard.component';
+import { PageWreslersListComponent } from './components/page-wreslers-list/page-wreslers-list.component';
+import { PageMatchCalendarComponent } from './components/page-match-calendar/page-match-calendar.component';
+import { PageMatchDetailsComponent } from './components/page-match-details/page-match-details.component';
+import { PageNewMatchComponent } from './components/page-new-match/page-new-match.component';
+import { PageRentWreslerComponent } from './components/page-rent-wresler/page-rent-wresler.component';
+import { PageWreslerDetailsComponent } from './components/page-wresler-details/page-wresler-details.component';
 
 
 const routes: Routes = [
@@ -14,8 +20,29 @@ const routes: Routes = [
     component: PageDashboardComponent
   },
   {
-
-  }
+    path: 'match-calendar',
+    component: PageMatchCalendarComponent
+  },
+  {
+    path: 'match-details',
+    component: PageMatchDetailsComponent
+  },
+  {
+    path: 'new-match',
+    component: PageNewMatchComponent
+  },
+  {
+    path: 'rent-wresler',
+    component: PageRentWreslerComponent
+  },
+  {
+    path: 'wresler-details',
+    component: PageWreslerDetailsComponent
+  },
+  {
+    path: 'wresler-list',
+    component: PageWreslersListComponent
+  },
 ];
 
 @NgModule({
