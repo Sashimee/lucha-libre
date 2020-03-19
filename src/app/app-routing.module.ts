197a1ff3bbd62b45d96a793e042e7b9e8c0dbd8c
GOStephano <60096607+GOStephano@users.noreply.github.com>
2020-03-19 14:55:23 +0100

    routerlink wrester id

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,52 +1,51 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
-import { PageDashboardComponent } from './components/page-dashboard/page-dashboard.component';
-import { PageWreslersListComponent } from './components/page-wreslers-list/page-wreslers-list.component';
-import { PageMatchCalendarComponent } from './components/page-match-calendar/page-match-calendar.component';
-import { PageMatchDetailsComponent } from './components/page-match-details/page-match-details.component';
-import { PageNewMatchComponent } from './components/page-new-match/page-new-match.component';
-import { PageRentWreslerComponent } from './components/page-rent-wresler/page-rent-wresler.component';
-import { PageWreslerDetailsComponent } from './components/page-wresler-details/page-wresler-details.component';
-
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
+import { PageDashboardComponent } from "./components/page-dashboard/page-dashboard.component";
+import { PageWreslersListComponent } from "./components/page-wreslers-list/page-wreslers-list.component";
+import { PageMatchCalendarComponent } from "./components/page-match-calendar/page-match-calendar.component";
+import { PageMatchDetailsComponent } from "./components/page-match-details/page-match-details.component";
+import { PageNewMatchComponent } from "./components/page-new-match/page-new-match.component";
+import { PageRentWreslerComponent } from "./components/page-rent-wresler/page-rent-wresler.component";
+import { PageWreslerDetailsComponent } from "./components/page-wresler-details/page-wresler-details.component";
 
 const routes: Routes = [
   {
-    path: '',
-    redirectTo: 'dashboard',
-    pathMatch: 'full'
+    path: "",
+    redirectTo: "dashboard",
+    pathMatch: "full"
   },
   {
-    path: 'dashboard',
+    path: "dashboard",
     component: PageDashboardComponent
   },
   {
-    path: 'match-calendar',
+    path: "match-calendar",
     component: PageMatchCalendarComponent
   },
   {
-    path: 'match-details',
+    path: "match-details",
     component: PageMatchDetailsComponent
   },
   {
-    path: 'new-match',
+    path: "new-match",
     component: PageNewMatchComponent
   },
   {
-    path: 'rent-wresler',
+    path: "rent-wresler",
     component: PageRentWreslerComponent
   },
   {
-    path: 'wresler-details',
+    path: "wresler-details/:wreslerId",
     component: PageWreslerDetailsComponent
   },
   {
-    path: 'wreslers-list',
+    path: "wreslers-list",
     component: PageWreslersListComponent
-  },
+  }
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
